174d02b48699621c5c729aca7ba246495c8ec111
lee <l_lpp@naver.com>
2024-12-12 18:06:06 +0900

    쉅 끝

--- ex02-js/scripts/bom.js
+++ ex02-js/scripts/bom.js
@@ -2,11 +2,13 @@
 
 let newWin;  
 
+
+// 새 창 열기 
 function openWindow() {
     // window.open("https://www.daum.net");
 
-    // let newWin;
-    // newwindow.open("https://www.daum.net", 
+
+    // window.open("https://www.daum.net", 
     //             "daum", 
     //             "width=400, height=400, left=100, top=100");
 
@@ -17,16 +19,29 @@ function openWindow() {
                 "width=400, height=400, left=100, top=100");
 }
 
+
+
+
+// 새 창 닫기
 function closeWindow() {
     if (newWin !== undefined && !newWin.closed)
         newWin.close();
 }
+/*
+newWin !== undefined : newWin 변수가 정의되어 있는지 확인 (newWin은 일반적으로 window.open() 메서드를 사용하여 새로운 브라우저 창을 열 때 반환되는 객체)
+//                       => newWin이 undefined가 아니어야만 실행된다!
+!newWin.closed : newWin.closed는 newWin이 열린 창이 아직 닫히지 않았는지 확인하는 속성
+//                       => newWin.closed는 창이 닫히면 true가 되고, 열린 상태이면 false
+//                       => !newWin.closed는 이 값이 false일 때 (즉, 창이 열려 있을 때)만 실행
+*/
 
 
 
 
+
+// 창 크기 확인 
 function printInner() {
-    let message = `현재 윈도우 창의 크기는 ` +
-    `${window.innerWidth} x ${window.innerHeight} 입니다.`;
+    let message = `현재 윈도우 창의 크기는` +
+    ` ${window.innerWidth} x ${window.innerHeight} 입니다.`;
     alert(message);
 }
